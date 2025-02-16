3dd927ad926f4096726df12c414d9a66ae042b20
Gnahiet.any <91592659+eosfel97@users.noreply.github.com>
2025-02-16 01:33:58 +0100

    modification projet et base saine  , projet denifitive : any

---
+++ src/components/Nav.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import "../assets/Nav.css";
+
+
+const Nav = () => {
+    return (
+        <header className="header">
+          <div className="top-bar">
+            <div className="top-bar__location">
+              <span className="top-bar__title">SCHOOL LOCATION</span>
+              <span className="top-bar__details">56 MAIN STREET / PARIS</span>
+            </div>
+            <div className="top-bar__hotline">
+              <span className="top-bar__title">HOTLINE</span>
+              <span className="top-bar__details">+33 26 25 65 23</span>
+            </div>
+          </div>
+    
+          <nav className="main-nav">
+            <div className="main-nav__logo">
+              <h1>voot kids</h1>
+            </div>
+            <ul className="main-nav__links">
+              <li><a href="#home">Home</a></li>
+              <li><a href="#about">About</a></li>
+              <li><a href="#pages">Pages</a></li>
+              <li><a href="#programs">Programs</a></li>
+              <li><a href="#contacts">Contacts</a></li>
+              <li><a href="#inscription">Inscription</a></li>
+            </ul>
+          </nav>
+        </header>
+      );
+    }
+
+export default Nav;
